8e05d804c9b768e1de9a2217bdb9f454d8535bce
horribleinternet <cyberblah@gmail.com>
2025-08-06 01:10:21 +0900

    server that doesn't find files

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	serverMux := http.NewServeMux()
+	server := http.Server{Handler: serverMux, Addr: ":8080"}
+	err := server.ListenAndServe()
+	fmt.Println(err)
 }
