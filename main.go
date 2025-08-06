81f5e93745251cdef09e4261edd1db88791f880f
horribleinternet <cyberblah@gmail.com>
2025-08-07 00:24:14 +0900

    server test file

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	serverMux := http.NewServeMux()
+	serverMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{Handler: serverMux, Addr: ":8080"}
 	err := server.ListenAndServe()
 	fmt.Println(err)
